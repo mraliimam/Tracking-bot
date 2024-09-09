c89a90857bd799cddf01d8f5e5f11e7cbdfa4b14
Ali Imam <mraliimam@gmail.com>
2024-09-10 00:48:28 +0500

    Persistent DB

--- market/__init__.py
+++ market/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/data/scrapper.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///var/data/scrapper.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['USE_ROLE_PERMISSIONS'] = False
