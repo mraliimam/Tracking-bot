5c5e2ee9ddad0d6101c5d00f9cf6e025a30099ad
Ali Imam <mraliimam@gmail.com>
2024-09-10 01:06:28 +0500

    DB Persist

--- market/__init__.py
+++ market/__init__.py
@@ -3,8 +3,20 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
+import logging
+logging.basicConfig(level=logging.INFO)
+# Start from the current working directory
+current_path = os.getcwd()
 
-DB_ROOT = os.path.join(os.sep, 'var', 'data')  # os.sep is '/' in Unix-like systems
+# Loop to go back until the root directory is reached
+while current_path != os.path.dirname(current_path):
+    logging.info(f"Current Path: {current_path}")
+    current_path = os.path.dirname(current_path)  # Go one level up
+
+# At this point, current_path should be the root '/'
+logging.info(f"Reached Root Directory: {current_path}")
+
+DB_ROOT = os.path.join(current_path, 'var', 'data')  # os.sep is '/' in Unix-like systems
 DATABASE_PATH = os.path.join(DB_ROOT, 'scrapper.db')
 
 app = Flask(__name__)
@@ -13,9 +25,9 @@ app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['USE_ROLE_PERMISSIONS'] = False
 
-import logging
 
-logging.basicConfig(level=logging.INFO)
+
+
 logging.info(f"Database path set to: {DATABASE_PATH}")
 
 
