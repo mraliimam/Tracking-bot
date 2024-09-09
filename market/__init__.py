b247492de846f30b4d2b4063584831c3f66fc5ab
Ali Imam <mraliimam@gmail.com>
2024-09-10 00:56:36 +0500

    Persistent DB

--- market/__init__.py
+++ market/__init__.py
@@ -2,12 +2,22 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
+
+DB_ROOT = os.path.join('/', 'var', 'data')  # This points to /var/data
+DATABASE_PATH = os.path.join(DB_ROOT, 'scrapper.db')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///var/data/scrapper.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_PATH}'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['USE_ROLE_PERMISSIONS'] = False
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.info(f"Database path set to: {DATABASE_PATH}")
+
 
 db = SQLAlchemy(app)
 
